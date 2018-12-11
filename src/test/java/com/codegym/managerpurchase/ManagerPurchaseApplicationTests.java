2a69836ad96bd4964d4444b9cc6400fd7c8744e3
TruongDepZai <nguyenxuantruong96.hust@gmail.com>
2018-12-11 15:44:40 +0700

    Commit file đầu tiên, nhánh master

---
+++ src/test/java/com/codegym/managerpurchase/ManagerPurchaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codegym.managerpurchase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagerPurchaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
